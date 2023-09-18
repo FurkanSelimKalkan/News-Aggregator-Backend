bbe0f919d84b41ad6af9bcb1f8217675762ab732
Furkan <furkan.selim1999@hotmail.com>
2023-09-19 00:23:58 +0200

    - Created API and Service for news - Created API Endpoints for future uses of secured endpoints - Configured CORS

---
+++ src/main/java/portfolio/demo/controller/ApiController.java
@@ -0,0 +1,32 @@
+package portfolio.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import portfolio.demo.service.NewsService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Autowired
+    private NewsService newsService;
+
+    @GetMapping("/public")
+    public String getMessage() {
+        return "Hello";
+    }
+
+    @GetMapping("/private")
+    public String secured() {
+        System.out.printf("Forbidden route works");
+        return "Hello, Secured";
+    }
+
+    @GetMapping("/news")
+    public String getNews() {
+        return newsService.getNews();
+    }
+}
