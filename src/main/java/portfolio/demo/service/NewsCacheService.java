2e6e5da1a2565c11d4fc2652c95e49a6d32d7211
Furkan <furkan.selim1999@hotmail.com>
2023-09-23 15:24:03 +0200

    - TTL refreshes after retrieving data from cache

--- src/main/java/portfolio/demo/service/NewsCacheService.java
+++ src/main/java/portfolio/demo/service/NewsCacheService.java
@@ -10,16 +10,22 @@ import java.util.concurrent.TimeUnit;
 public class NewsCacheService {
 
     private static final String CACHE_KEY = "news_data";
+    private static final int expirationSeconds = 120;
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     public void cacheNews(String newsData) {
-        redisTemplate.opsForValue().set(CACHE_KEY, newsData, 120, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set(CACHE_KEY, newsData, expirationSeconds, TimeUnit.SECONDS);
     }
 
     public String getNews() {
+        refreshCacheTTL();
         return (String) redisTemplate.opsForValue().get(CACHE_KEY);
+    }
+
+    public void refreshCacheTTL() {
+        redisTemplate.expire(CACHE_KEY, expirationSeconds, TimeUnit.SECONDS);
     }
 
     public void removeNews() {
